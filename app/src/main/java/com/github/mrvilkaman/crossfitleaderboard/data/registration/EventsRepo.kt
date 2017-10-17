254c1c662c65524e6d8f27e8e96c103b739e6658
Zahar <zahar.zolotarev@gmail.com>
2017-10-17 15:24:36 +0300

    added event stub

--- app/src/main/java/com/github/mrvilkaman/crossfitleaderboard/data/registration/EventsRepo.kt
+++ app/src/main/java/com/github/mrvilkaman/crossfitleaderboard/data/registration/EventsRepo.kt
@@ -1,7 +1,8 @@
 package com.github.mrvilkaman.crossfitleaderboard.data.registration
 
-import com.github.mrvilkaman.crossfitleaderboard.business.CrossfitEvent
+import com.github.mrvilkaman.crossfitleaderboard.business.*
 import com.github.mrvilkaman.crossfitleaderboard.repository.EventsRepo
+import java.util.*
 import javax.inject.Inject
 
 
@@ -10,4 +11,28 @@ constructor() : EventsRepo {
     override var currentEvent: CrossfitEvent? = null
 //        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
 //        set(value) {}
+}
+
+
+class EventsRepoStub @Inject
+constructor() : EventsRepo {
+    override var currentEvent: CrossfitEvent? = null
+        get() {
+            val results = arrayListOf(
+                    CrossfitResult(CrossfitTeam("Username 1")),
+                    CrossfitResult(CrossfitTeam("Username 2")),
+                    CrossfitResult(CrossfitTeam("Username 3"))
+            )
+            val results2 = arrayListOf(
+                    CrossfitResult(CrossfitTeam("Username 1")),
+                    CrossfitResult(CrossfitTeam("Username 2")),
+                    CrossfitResult(CrossfitTeam("Username 3"))
+            )
+            val wodsCrossfit = arrayListOf(
+                    CrossfitWod(1, "Бурпи и еще раз бурпи", results, WodType.AMRAP),
+                    CrossfitWod(2, "21-15-9\nТрастеры\nБурпи", results2, WodType.AFAP)
+            )
+
+            return CrossfitEvent("Заголовок!", Date(), wodsCrossfit)
+        }
 }
--- app/src/main/java/com/github/mrvilkaman/crossfitleaderboard/di/app/AppRepoModules.kt
+++ app/src/main/java/com/github/mrvilkaman/crossfitleaderboard/di/app/AppRepoModules.kt
@@ -1,6 +1,6 @@
 package com.github.mrvilkaman.crossfitleaderboard.di.app
 
-import com.github.mrvilkaman.crossfitleaderboard.data.registration.EventsRepoImpl
+import com.github.mrvilkaman.crossfitleaderboard.data.registration.EventsRepoStub
 import com.github.mrvilkaman.crossfitleaderboard.repository.EventsRepo
 import dagger.Binds
 import dagger.Module
@@ -11,5 +11,6 @@ import javax.inject.Singleton
 abstract class AppRepoModules {
     @Singleton
     @Binds
-    abstract fun provideEventsRepo(repo: EventsRepoImpl): EventsRepo
+//    abstract fun provideEventsRepo(repo: EventsRepoImpl): EventsRepo
+    abstract fun provideEventsRepo(repo: EventsRepoStub): EventsRepo
 }
